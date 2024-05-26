524c5cc73285bca34663bc691bd8706022a7e8c2
rubensalado <ruben@videohead.io>
2024-05-26 11:58:59 +0200

    change demo url

--- assets/js/demo.js
+++ assets/js/demo.js
@@ -4,5 +4,5 @@ document.getElementById("demo-form").addEventListener("submit", (e) => {
   const url = document.getElementById("demo-url").value;
   const email = document.getElementById("demo-email").value;
 
-  window.location.href = `http://localhost:3000/demo?url=${encodeURIComponent(url)}&email=${encodeURIComponent(email)}`;
+  window.location.href = `http://app.videohead.io/demo?url=${encodeURIComponent(url)}&email=${encodeURIComponent(email)}`;
 });
