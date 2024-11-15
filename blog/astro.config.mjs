f5cd384c2a1625323f3dad3afe52dc6661ffa07b
rsaladocid <rsaladocid@gmail.com>
2024-11-15 11:40:04 +0100

    use absolute paths

--- blog/astro.config.mjs
+++ blog/astro.config.mjs
@@ -7,7 +7,7 @@ import { remarkReadingTime } from './src/utils/readTime.ts'
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://videohead.io/blog',
-	base: '/blog',
+	base: 'blog',
 	markdown: {
 		remarkPlugins: [remarkReadingTime],
 		drafts: true,
